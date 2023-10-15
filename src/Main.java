0bc4869495f62799a6c89fc72f68d231edc62e66
Andrey.Skripay <lordktan@gmail.com>
2023-10-15 12:52:17 +0500

    Доработаная Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         // Задание 1
-        int year = 2024; // Здесь можно изменить значение года
-
-        if (isLeapYear(year)) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
-        }
+        int year = 2021; // год для проверки
+        checkLeapYear(year);
     }
 
-    public static boolean isLeapYear(int year) {
+    public static void checkLeapYear(int year) {
         if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            return true;
+            System.out.println(year + " год — високосный год");
         } else {
-            return false;
+            System.out.println(year + " год — невисокосный год");
         }
     }
 }
+
